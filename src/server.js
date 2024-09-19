6cb529927005e147963b09569a085e23f3fcc088
Ifetilayo <ifetilayoade@gmail.com>
2024-09-19 17:13:19 -0500

    fixed post call issue

--- src/server.js
+++ src/server.js
@@ -6,24 +6,12 @@ const app = express();
 const corsOptions = {
   origin: "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, //access-control-allow-credentials:true
+  credentials: true,
   optionSuccessStatus: 200,
 };
 
 app.use(express.json());
 app.use(cors(corsOptions));
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000/");
-
-//   // res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
@@ -68,9 +56,12 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   const article = await db.collection("articles").findOne({ name: name });
 
   if (article) {
+    res.json(article);
   } else res.send(`${name} article doesn\'t exist`);
 });
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Go catch the server at PORT ${process.env.PORT || 8000}`);
-});
+connectToDb(() =>
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Go catch the server at PORT ${process.env.PORT || 8000}`);
+  })
+);
