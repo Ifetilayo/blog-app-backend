648b65125d4d31654314ed5c73c684cb2854f0b0
Ifetilayo <ifetilayoade@gmail.com>
2024-09-19 14:35:33 -0500

    init repo

---
+++ src/server.js
@@ -0,0 +1,74 @@
+import express from "express";
+import { db, connectToDb } from "./db.js";
+import cors from "cors";
+
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000/");
+
+//   // res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.get("/api/articles/:name", async (req, res) => {
+  const { name } = req.params;
+
+  const article = await db.collection("articles").findOne({ name: name });
+
+  if (article) {
+    res.json(article);
+  } else res.sendStatus(404);
+});
+
+app.put("/api/articles/:name/upvote", async (req, res) => {
+  const { name } = req.params;
+
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $inc: { upvotes: 1 },
+    }
+  );
+
+  const article = await db.collection("articles").findOne({ name: name });
+  if (article) {
+    res.json(article);
+  } else res.sendStatus(404);
+});
+
+app.post("/api/articles/:name/comments", async (req, res) => {
+  const { name } = req.params;
+  if (req.body) {
+    const { postedBy, text } = req.body;
+
+    await db.collection("articles").updateOne(
+      { name },
+      {
+        $push: { comments: { postedBy, text } },
+      }
+    );
+  } else {
+    res.sendStatus(400);
+  }
+  const article = await db.collection("articles").findOne({ name: name });
+
+  if (article) {
+  } else res.send(`${name} article doesn\'t exist`);
+});
+
+connectToDb(() => app.listen(8000, () => console.log("Server listening!!!")));
